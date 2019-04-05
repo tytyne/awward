ff14dff3ef472b6508c51311ab7093599cff50cb
tytyne <dusaflora2@gmail.com>
2019-04-05 13:15:28 +0200

    add name and occupation in classe profile

--- photos/models.py
+++ photos/models.py
@@ -73,7 +73,9 @@ class PhotosLetterRecipients(models.Model):
    
   
 class Profile(models.Model):
+    name=models.TextField(max_length = 10, blank = True, null = True)
     profile_pic = models.ImageField(upload_to = 'profile_pic/', null = True)
+    occupation=models.TextField(max_length = 10, blank = True, null = True)
     bio = models.TextField(max_length = 500, blank = True, null = True)
     user = models.OneToOneField(User, on_delete = models.CASCADE, null = True)
 
